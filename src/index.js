c22dbafe85dc9898e5414160243312940d19ef63
Salie Lim <salie.lim@gmail.com>
2017-09-07 19:11:18 +0800

    Add realtime updates using GraphQL subscriptions

--- src/index.js
+++ src/index.js
@@ -6,10 +6,23 @@ import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
 import { BrowserRouter } from 'react-router-dom'
 import { GC_AUTH_TOKEN } from './constants'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj78r1u1m13ej0189tqitxeqr'
 })
+
+const wsClient = new SubscriptionClient('wss://subscriptions.ap-northeast-1.graph.cool/v1/cj78r1u1m13ej0189tqitxeqr', {
+  reconnect: true,
+  connectionParams: {
+     authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
 
 networkInterface.use([{
   applyMiddleware(req, next) {
@@ -23,7 +36,7 @@ networkInterface.use([{
 }])
 
 const client = new ApolloClient({
-  networkInterface
+  networkInterface: networkInterfaceWithSubscriptions
 })
 
 ReactDOM.render(
